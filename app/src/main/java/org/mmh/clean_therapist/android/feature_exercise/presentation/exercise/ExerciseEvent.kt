b10971da2b8763aab787734f3440efb4f3abec2c
shanjidhasan <48083980+shanjidhasan@users.noreply.github.com>
2023-03-17 12:48:34 +0600

    Exercise Activity

---
+++ app/src/main/java/org/mmh/clean_therapist/android/feature_exercise/presentation/exercise/ExerciseEvent.kt
@@ -0,0 +1,6 @@
+package org.mmh.clean_therapist.android.feature_exercise.presentation.exercise
+
+sealed class ExerciseEvent {
+    object FlipCamera : ExerciseEvent()
+    object GoToAssessmentPage : ExerciseEvent()
+}
---
+++ app/src/main/java/org/mmh/clean_therapist/android/feature_exercise/presentation/guideline/GuidelineViewModel.kt
@@ -0,0 +1,16 @@
+package org.mmh.clean_therapist.android.feature_exercise.presentation.guideline
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import org.mmh.clean_therapist.android.feature_authentication.domain.model.Patient
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GuidelineViewModel @Inject constructor(
+) : ViewModel() {
+
+    private val _patient = mutableStateOf<Patient?>(null)
+    val patient: State<Patient?> = _patient
+}
